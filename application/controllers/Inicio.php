39d8405b95b57e83194b981e3f780de809944785
mogurbon@gmail.com <mogurbon@gmail.com>
2016-07-14 16:02:04 -0500

    adding jquery ,jquery layout , views

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -3,27 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 use Abraham\TwitterOAuth\TwitterOAuth;
 class Inicio extends CI_Controller {
     
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	
 	public function index()
 	{
             
-            $connection = new TwitterOAuth(CONSUMERKEY, CONSUMERSECRET, ACCESTOKEN, ACCESTOKENSECRET);
-            $content = $connection->get("account/verify_credentials");
+        $connection = new TwitterOAuth(CONSUMERKEY, CONSUMERSECRET, ACCESTOKEN, ACCESTOKENSECRET);
+        $content = $connection->get("account/verify_credentials");
+        $search=$connection->get('search/tweets', ['q'=>'ramsay']);
+        $this->load->helper('url');
             
-		$this->load->view('welcome_message');
+		$this->load->view('index_view');
 	}
 }
+
+
+
