acd67a6faeb58cccdc434ee9db31068905ab6758
mogurbon@gmail.com <mogurbon@gmail.com>
2016-07-15 14:49:04 -0500

    getting the principal layout

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -7,9 +7,9 @@ class Inicio extends CI_Controller {
 	public function index()
 	{
             
-        $connection = new TwitterOAuth(CONSUMERKEY, CONSUMERSECRET, ACCESTOKEN, ACCESTOKENSECRET);
+        /*$connection = new TwitterOAuth(CONSUMERKEY, CONSUMERSECRET, ACCESTOKEN, ACCESTOKENSECRET);
         $content = $connection->get("account/verify_credentials");
-        $search=$connection->get('search/tweets', ['q'=>'ramsay']);
+        $search=$connection->get('search/tweets', ['q'=>'ramsay']);*/
         $this->load->helper('url');
             
 		$this->load->view('index_view');
